af2330cbefe1545c24c33377b3655c89ce98d0ff
samuelmartingc <samuelmartingc@gmail.com>
2017-01-25 19:46:28 +0100

    altitude updates

--- src/main/scala/machines/Nautilus.scala
+++ src/main/scala/machines/Nautilus.scala
@@ -11,11 +11,17 @@ object Nautilus {
 class Nautilus extends Actor with ActorLogging {
   import Altimeter._
   import Nautilus._
+  import EventSource._
+  override def preStart() {
+    altimeter ! RegisterListener(self)
+  }
   val altimeter = context.actorOf(
     Props[Altimeter], "Altimeter")
   val controls = context.actorOf(
     Props(new ControlSurfaces(altimeter)), "ControlSurfaces")
   def receive = {
+    case AltitudeUpdate(altitude) =>
+      log info(s"Altitude is now: $altitude")
     case GiveMeControl =>
       log info("Nautilus giving control.")
       sender ! controls
